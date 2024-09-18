00e36c747f47b62c21393bd012897ef76e48462e
ModernGalahad <m.git@dv.dx.ro>
2024-09-18 15:14:43 +0300

    Finished Ch 3 Exercise 2 and 3. Learned logical opposites

--- Ch 3/2.py
+++ Ch 3/2.py
@@ -1,10 +1,12 @@
-starting_day = int(input("What is the starting day of your holiday? 1 is Sunday. Please enter a number from 1 to 7:  "))
+starting_day = int(input("What is the starting day of your holiday? 1 is Sunday. Please enter a number from 1 to 7:  ")) - 1
 days_away = int(input("How many days will your holiday last?"))
+
 days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
-weeks = days_away // 7
-return_day_nr = days_away % 7
+return_day = (days_away + starting_day) % 7
 
-day_text = days[starting_day -1]
+return_day_text = days[return_day]
 
-print(day_text)
+print(return_day_text)
+
+#3 ( wed) is starting day. I leave for 19 days. 
